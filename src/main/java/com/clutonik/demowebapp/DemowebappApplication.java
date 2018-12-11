f20df9d7909dd3c04eafb6067decd59408dfcea1
Sahil Chawla <sahil.chawla@clutonik.com>
2018-12-10 21:59:16 -0500

    First Demo Spring Project

---
+++ src/main/java/com/clutonik/demowebapp/DemowebappApplication.java
@@ -0,0 +1,12 @@
+package com.clutonik.demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebappApplication.class, args);
+	}
+}
---
+++ src/main/java/com/clutonik/demowebapp/model/Author.java
@@ -0,0 +1,41 @@
+package com.clutonik.demowebapp.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new Hashset<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
